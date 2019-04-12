abbb07d31db5eedbe612676302f607a39ded9c08
文昊 胡 <hwh19981124@hotmail.com>
2019-04-12 10:58:14 +0800

    Matrix

--- Ex3.1/Main.cpp
+++ Ex3.1/Main.cpp
@@ -28,6 +28,40 @@ private:
 
 std::vector<Point2D<int>> GraphData;
 
+template <typename T>
+class Matrix3by3
+{
+public:
+	Matrix3by3()
+		:
+		dimx(3),
+		dimy(3)
+	{
+		inner.resize(dimx*dimy);
+	}
+
+	//forget this
+	Matrix3by3(unsigned int dimx_, unsigned int dimy_)
+		:
+		dimx(dimx_),
+		dimy(dimy_)
+	{
+		inner.resize(dimx*dimy);
+	}
+
+	T& getElem(unsigned int x, unsigned int y) {
+		if (x>=dimx||y>=dimy)
+		{
+			throw std::out_of_range("Matrix out of range");
+		}
+		return inner[dimx*y + x];
+	}
+
+
+private:
+	std::vector<T> inner;
+	unsigned int dimx, dimy;
+};
 
 
 void InitTriangle();
